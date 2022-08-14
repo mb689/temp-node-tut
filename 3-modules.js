fbe4734f10998ae42a44405a809b5cec24a653df
Mohamed <mboudjatat17@hotmail.com>
2022-08-14 19:12:51 +0100

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+//Modules
+const {john, peter} = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
